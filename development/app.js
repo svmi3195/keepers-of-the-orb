1b2d5304d361704e6d585bdc7aec95c12159a1e6
svmi3195 <target1983@gmail.com>
2017-05-27 16:15:05 +0300

    basic tilemap

--- development/app.js
+++ development/app.js
@@ -7,7 +7,34 @@ function runGame(){
     var canvas = document.getElementById('game');
     var ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 4; //-4 because of stupid scrollbars
+    canvas.height = window.innerHeight;
 
+    var map = new Tilemap(5,5, ctx);
+    map.render();
 
-}//end of runGame
+};//end of runGame
+
+function Tilemap(cols, rows, contex) {
+    this.cols = cols;
+    this.rows = rows;
+    this.tsize = 40; //tile size (40 x 40)
+    this.tiles = [];
+    for(var i = 0; i < this.cols * this.rows; i++){
+        this.tiles.push(0);
+    };
+    this.getTile = function(col, row) {
+        return this.tiles[row * this.cols + col]
+    };
+    this.render = function(){
+        for (var c = 0; c < this.cols; c++) {
+            for (var r = 0; r < this.rows; r++) {
+                var tile = this.getTile(c, r);
+                if (tile == 0) {   
+                    contex.fillRect(c * this.tsize, r * this.tsize, this.tsize, this.tsize);
+                    contex.strokeStyle = 'white';
+                    contex.strokeRect(c * this.tsize, r * this.tsize, this.tsize, this.tsize);        
+                }
+            }
+        }
+    }
+};//end of Tilemap
