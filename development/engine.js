1403497b8f9830753198dd8b2e4e68015c783164
svmi3195 <target1983@gmail.com>
2017-06-12 18:43:09 +0300

    started working on pathfinder

--- development/engine.js
+++ development/engine.js
@@ -1,3 +1,14 @@
+function Tile(index, cols, rows){
+   this.index = index;
+   this.num = 0;
+   this.f = 0;
+   this.g = 0;
+   this.h = 0;
+   this.neighbors = [index - 1, index + 1, index - cols, index + cols].filter(function(x){
+     return x > 0 && x < cols * rows;
+   })
+};
+
 function Tilemap(canvas, context) {    
     this.tsize = 40; //tile size (40 x 40)
     this.cols = Math.ceil(canvas.width / this.tsize);
@@ -5,7 +16,7 @@ function Tilemap(canvas, context) {
 
     this.tiles = [];
     for(var i = 0; i < this.cols * this.rows; i++){
-        this.tiles.push(2);
+        this.tiles.push(new Tile(i, this.cols, this.rows));
     };
 
     this.textures = [];         
@@ -21,12 +32,12 @@ function Tilemap(canvas, context) {
         for (var c = 0; c < this.cols; c++) {
             for (var r = 0; r < this.rows; r++) {
                 var tile = this.getTile(c, r);
-                if (tile == 1) {                    
+                if (tile.num == 1) {                    
                     context.fillStyle = 'green';   
                     context.fillRect(c * this.tsize, r * this.tsize, this.tsize, this.tsize);                    
-                }else if (tile == 2){
+                }else if (tile.num == 2){
                     context.drawImage(this.textures[0], c * this.tsize, r * this.tsize);
-                }else if (tile == 3){
+                }else if (tile.num == 3){
                     context.fillStyle = 'gray';   
                     context.fillRect(c * this.tsize, r * this.tsize, this.tsize, this.tsize);
                 }
@@ -44,9 +55,10 @@ function Tilemap(canvas, context) {
     };// end of Tilemap render grid
 
     this.update = function(col, row, value){
-        this.tiles[row * this.cols + col] = value;
+        this.tiles[row * this.cols + col].num = value;
     }
 
+    //why do we even need cols and rows here? rewrite with tiles array!
     this.randomize = function(){
         for (var c = 0; c < this.cols; c++) {
             for (var r = 0; r < this.rows; r++) {
@@ -64,3 +76,74 @@ function Tilemap(canvas, context) {
     };//end of randomize map
 
 };//end of Tilemap
+
+//PATHFINDER (work in progress)
+//https://codepen.io/svmi3195/pen/gRrBBJ
+
+var map = new Tilemap(); //change later to be pathfinder argument
+
+//transforms index of 2d array to index of 1d array
+function transIndex(arr){
+  //arr[0] - cols
+  //arr[1] - rows
+  return arr[0] * map.cols + arr[1];
+}
+
+function removeFromArray(array, element){
+  for(var i = array.length - 1; i >= 0; i--){
+    if(array[i] == element){
+      array.splice(i, 1);
+    }
+  }
+}
+
+function findPath(start, goal){
+  //grid equals map.tiles
+  //map.update(0, 0, 2); //makes start red
+  //map.update(map.cols - 1, map.rows - 1, 2); //makes end red
+  var openList = [];
+  var closedList = [];
+  if(!start){start = [0,0]}
+  if(!goal){goal = [map.cols - 1, map.rows - 1]}
+  
+  openList.push(start);
+  
+  if(openList.length > 0){
+    var winner = 0;
+    for(var i = 0; i < openList.length; i++){
+      if(map.tiles[transIndex(openList[i])].f < map.tiles[transIndex(openList[winner])].f){
+        winner = i;
+      }
+    }
+    var current = openList[winner];
+    
+    if(transIndex(current) == transIndex(goal)){
+      //console.log('done!')
+    }
+    
+    removeFromArray(openList, current);
+    closedList.push(current);
+    
+    //console.log(map.tiles[transIndex(current)].neighbors)
+    
+    var neighbors = map.tiles[transIndex(current)].neighbors;
+    //console.log(neighbors)
+    for(var nb = 0; nb < neighbors.length; nb++){
+      //var neighbor = neighbors[nb];
+    }
+    
+  }else{
+    //no solution
+  }
+  
+  //visualize:
+  //draw openList tiles as green
+  //and closedList tiles as red
+  for(var o = 0; o < openList.length; o++){
+    map.tiles[transIndex(openList[o])].num = 2;
+  }
+  for(var c = 0; c < closedList.length; c++){
+    map.tiles[transIndex(closedList[c])].num = 3;
+  }
+  
+}//end of findPath
