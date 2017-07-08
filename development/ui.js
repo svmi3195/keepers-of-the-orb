02b0371ab8865275a821b12c1559d593c22b32e4
svmi3195 <target1983@gmail.com>
2017-07-08 14:13:44 +0300

    added some basic styling to ui

--- development/ui.js
+++ development/ui.js
@@ -6,6 +6,12 @@ function UI(tilemap, context){
 
     this.selected = null;
 
+    this.themes = [{id: 0, name: 'black-golden', colors: ['#1a1a00', '#D4AF37']}, 
+                   {id: 1, name: 'black-silver', colors: ['#1a1a1a', '#c0c0c0']},
+                   {id: 2, name: 'black-purple', colors: ['#1a0033', '#bb33ff']}];
+    this.backgroundColor = this.themes[0].colors[0];
+    this.mainColor = this.themes[0].colors[1];
+
     this.select = function(obj){
         if(obj == this.selected){
             this.deselect();
@@ -21,8 +27,11 @@ function UI(tilemap, context){
     };
 
     this.render = function(){
-        context.fillStyle = 'magenta';   
+        context.fillStyle = this.backgroundColor;   
         context.fillRect(this.x, this.y, this.width, this.height);
+        context.strokeStyle = this.mainColor;
+        context.lineWidth = 3;
+        context.strokeRect(this.x, this.y, this.width, this.height);
 
         if(this.selected){
             var text = this.selected.name;
@@ -35,9 +44,9 @@ function UI(tilemap, context){
     };
 
     this.drawText = function(text){
-        context.fillStyle = 'black';
-        context.font = '16px serif';
-        context.fillText(text, this.x + 10, this.y + 20);
+        context.fillStyle = this.mainColor;
+        context.font = '20px serif';
+        context.fillText(text, this.x + 10, this.y + 25);
     };
     
 };
