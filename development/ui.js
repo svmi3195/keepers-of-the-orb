3de01788f32f7220df7c6bcbcd1676b0c61a3e19
svmi3195 <target1983@gmail.com>
2017-09-21 14:50:39 +0300

    rect for info screen

--- development/ui.js
+++ development/ui.js
@@ -5,7 +5,13 @@ function UI(tilemap, context){
     this.y = (tilemap.rows - 2) * tilemap.tsize + 5;
 	this.buttons = [];
 
-    this.selected = null;	
+    this.selected = null;
+    this.showingInfo = false;
+    this.showingInventory = false;
+    
+    //later store user-defined coords (by dragging)
+    this.infoCoords = {x: 10, y: 10};
+    this.inventoryCoords = {x: 10, y: 10};
 	
     this.themes = [{id: 0, name: 'black-golden', colors: ['#1a1a00', '#D4AF37']}, 
                    {id: 1, name: 'black-silver', colors: ['#1a1a1a', '#c0c0c0']},
@@ -25,7 +31,9 @@ function UI(tilemap, context){
 
     this.deselect = function(){
         this.selected = null;
-		this.buttons = [];
+        this.buttons = [];
+        this.showingInfo = false;
+        this.showingInventory = false;
     };
 
     this.render = function(){
@@ -50,27 +58,42 @@ function UI(tilemap, context){
 		
 		if(this.buttons.length != 0){
 			this.drawButtons();
-		}
+        }
+        
+        if(this.showingInfo){
+            this.drawInfoScreen();
+        }
     };
 
     this.drawText = function(text){
         context.fillStyle = this.mainColor;
         context.font = '20px serif';
         context.fillText(text, this.x + 10, this.y + 22);
-    };
-	
-	this.createButtons = function(buttons){
-		for(var i = 0; i < buttons.length; i++){
-			var btn = new Button(buttons[i], [this.x + this.width + i * 35 + 5, this.y]);
-			this.buttons.push(btn);			
-		}
-	};
+    };	
 
 	this.drawButtons = function(){
 		for(var i = 0; i < this.buttons.length; i++){
 			context.drawImage(this.buttons[i].texture, this.buttons[i].x, this.buttons[i].y);
 		}
-    }
+    };
+
+    this.drawInfoScreen = function(){
+        var width = 300;
+        var height = 300;
+
+        context.fillStyle = this.backgroundColor;
+        context.fillRect(this.infoCoords.x, this.infoCoords.y, width, height);
+        context.strokeStyle = this.mainColor;
+        context.lineWidth = 3;
+        context.strokeRect(this.infoCoords.x, this.infoCoords.y, width, height);
+    };
+
+    this.createButtons = function(buttons){
+		for(var i = 0; i < buttons.length; i++){
+			var btn = new Button(buttons[i], [this.x + this.width + i * 35 + 5, this.y]);
+			this.buttons.push(btn);			
+		}
+	};
     
     this.btnPressed = function(btn){
         switch(btn.type){
@@ -82,6 +105,13 @@ function UI(tilemap, context){
                 this.buttons = [];
                 if(this.selected.name == 'Mage'){
                     this.createButtons(['shoot', 'i', 'go', 'rune']);
+                }
+                break;
+            case 'i':
+                if(this.showingInfo){
+                    this.showingInfo = false;
+                }else{
+                    this.showingInfo = true;
                 }
                 break;
         }
